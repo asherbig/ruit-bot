21c372d04b3eaa1dbddad9075ca104ff65d3d048
Alex Herbig <aherbigs@gmail.com>
2018-02-05 16:49:24 -0500

    fixed untracked files

---
+++ js/credentials template.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'client_id': 'CLIENT ID HERE',
+    'client_secret': 'CLIENT SECRET HERE',
+    'oauth_access_token': 'OAUTH ACCESS TOKEN HERE',
+    'bot_oauth_token': 'BOT OAUTH ACCESS TOKEN HERE',
+  }
